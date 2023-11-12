d5572b77e107d9fcf7c5ff354994fafe578e754c
ZeroCool989 <>
2023-11-12 17:36:03 +0000

    Add basic routing for Home, Sign In, and Sign Up pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 
 function App() {
     return (
         <div className={styles.App}>
             <NavBar />
-            
+            <Container>
+                <Switch>
+                    <Route exact path ='/' render={() =><h1>Home</h1>}/>
+                    <Route exact path ='/signin' render={() =><h1>Sign in</h1>}/>
+                    <Route exact path ='/signup' render={() =><h1>Sign up</h1>}/>
+                </Switch>
+            </Container>
         </div>
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { Navbar as BootstrapNavbar, Container, Nav } from "react-bootstrap";
-import logo from '../assets/logo.png'
-import styles from '../styles/NavBar.module.css'
+import logo from '../assets/logo.png';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <BootstrapNavbar className={styles.NavBar} bg="light" expand="lg">
             <Container>
-                <BootstrapNavbar.Brand>
-                    <img src={logo} alt="logo" height="45" />
-                </BootstrapNavbar.Brand>
+                <NavLink to="/">
+                    <BootstrapNavbar.Brand>
+                        <img src={logo} alt="logo" height="45" />
+                    </BootstrapNavbar.Brand>
+                </NavLink>
                 <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
                 <BootstrapNavbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
-                        <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-                        <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
-                        <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/">
+                            <i className="fas fa-home"></i> Home
+                        </NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin"><i className="fas fa-sign-in-alt"></i>
+                            Sign in</NavLink>
+                        <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signup">
+                            <i className="fas fa-user-plus"></i> Sign up
+                        </NavLink>
                     </Nav>
                 </BootstrapNavbar.Collapse>
             </Container>
