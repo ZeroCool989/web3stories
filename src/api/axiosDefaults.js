ab2fa4c94abaffec4a9e1de92d56613fb6c2fda2
ZeroCool989 <>
2023-12-02 11:36:34 +0000

    Add SignInForm component with basic structure and styling

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,4 +1,8 @@
 import axios from "axios";
-axios.defaults.baseURL ='https://web3stories-2f921fb41d11.herokuapp.com';
-axios.defaults.headers.post['Content-Type']='multipart/form-data';
-axios.defaults.withCredentials=true;
+
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
